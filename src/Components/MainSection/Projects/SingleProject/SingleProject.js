eceb3e382b8db594c82e993cb41c6d022b3efd46
Carlos <carlos.escobar_uk@live.com>
2020-12-28 14:00:33 +0000

    Changes in Css and deployment in Firebase

--- src/Components/MainSection/Projects/SingleProject/SingleProject.js
+++ src/Components/MainSection/Projects/SingleProject/SingleProject.js
@@ -4,7 +4,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const SingleProject = (props) => {
 	return (
 		<div className="card">
-			{/* front of card */}
+			<div className="card-links-container">
+				{props.projectLinks.map((l, index) => {
+					return (
+						<a key={index} href={l.link} rel="noreferrer" target="_blank">
+							<FontAwesomeIcon
+								key={index}
+								className="back-card-link"
+								icon={l.icon}
+							/>
+						</a>
+					);
+				})}
+			</div>
 			<div className="card-side card-front">
 				<img src={props.projectImg} alt="budge-app" />
 				<div className="card-info">
@@ -14,7 +26,6 @@ const SingleProject = (props) => {
 						<br />
 						<p>{props.projectDescription2}</p>
 					</div>
-					<h5>more info? just flip!</h5>
 					<div className="footer-languages">
 						<h4>Language</h4>
 						{props.projectLanguages.map((l) => (
@@ -28,22 +39,7 @@ const SingleProject = (props) => {
 				</div>
 			</div>
 			{/* back of card */}
-			<div className="card-side card-back">
-				<div className="card-links-container">
-					<h4>Visit</h4>
-					{props.projectLinks.map((l, index) => {
-						return (
-							<a key={index} href={l.link} rel="noreferrer" target="_blank">
-								<FontAwesomeIcon
-									key={index}
-									className="back-card-link"
-									icon={l.icon}
-								/>
-							</a>
-						);
-					})}
-				</div>
-			</div>
+			<div className="card-side card-back"></div>
 		</div>
 	);
 };
